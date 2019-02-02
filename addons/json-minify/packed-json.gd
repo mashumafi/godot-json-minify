60a9071e393cfbc17ba204ae3b0085f864e31a04
Matthew Murphy <mashumafi@users.noreply.github.com>
2019-02-02 17:27:38 -0500

    Clear data (#2)
    * adding testing
    
    * add some unit and integration tests
    
    * update test name
    
    * always use cache and clear __data__ after instancing

--- addons/json-minify/packed-json.gd
+++ addons/json-minify/packed-json.gd
@@ -20,20 +20,15 @@ func set_data(filename):
 
 var __cache__ = null
 
-func get_data(duplicate=false):
-	if null == __cache__:
-		__cache__ = instance()
-	var result = __cache__
-	if duplicate:
-		result = result.duplicate()
-	return result
-
 func instance():
-	var data = __data__.get_string_from_utf8()
-	var parsed_json = JSON.parse(data)
-	if OK != parsed_json.error:
-		var format = [parsed_json.error_line, parsed_json.error_string]
-		var error_string = "%d: %s" % format
-		print(error_string)
-		return null
-	return parsed_json.result
+	if null == __cache__:
+		var data = __data__.get_string_from_utf8()
+		__data__.resize(0) # free the data
+		var parsed_json = JSON.parse(data)
+		if OK != parsed_json.error:
+			var format = [parsed_json.error_line, parsed_json.error_string]
+			var error_string = "%d: %s" % format
+			print(error_string)
+			return null
+		__cache__ = parsed_json.result
+	return __cache__
